61475b6201f2c944a6d503d8bda2df82c24d0a96
James Priebe <jamespriebe92@gmail.com>
2017-08-28 17:58:54 -0400

    more accurate tokenization, scoring system to reduce false positives

--- feature_match.py
+++ feature_match.py
@@ -18,14 +18,12 @@ def create_dict(fname):
 
     with open(fname) as f:
         content = f.readlines()
-        print ('number of listings: %d' % (len(content)))
+        #print ('number of listings: %d' % (len(content)))
 
         for c in content:
             item = json.loads(c)
-            item['matched'] = False
             manufacturer_string = item['manufacturer']
             key = format_key(manufacturer_string)
-            #del item['manufacturer']
             item_dict.setdefault(key,[]).append(item)
     print('number of unique manufacturers: %d' % (len(item_dict)))   
     return item_dict
@@ -45,173 +43,136 @@ def create_list(fname):
 def format_key(raw_key):
     # strip whitespace, special characters from key and convert to lowercase
     # helps with formatting mismatches between product and listing manufacturers
-    formatted_key = (''.join(c for c in raw_key if c.isalnum())).lower()
+    formatted_key = (''.join(c for c in raw_key if c.isalnum() or c == '.')).lower()
     return formatted_key
 
 def tokenize(string):
-    tokens = [keyword.lower() for keyword in re.split('-| ',string)]
+    #token = (''.join(c for c in string if c.isalnum())).lower()
+    #return token
+
+    # #tokens = [format_key(token) for token in re.split('-| |_',string)]
+    tokens = [format_key(token) for token in string.split(' ')]
+
     return tokens
 
 
 
 
-def find_matches_deep(product, listings):
-
-
-    #print("product name: %s" % (product.get('product_name')))
-
-    matches = []
-
-
-    # correct listing must be from same manufacturer and contain product model and family in title
-    product_key = format_key(product['manufacturer'])
-    product_keywords = tokenize(product['model']) + tokenize(product.get('family', ''))#+ tokenize(product['family'])
-
-
-    # now we check all keys which are a subset of the product key or vice versa
-    for listing_key in listings:
-
-        if (listing_key in product_key or product_key in listing_key ):
-            possible_matches = listings.get(listing_key, [])
-            for listing in possible_matches:#listings[key]:
-                if not listing['matched']:
-                    #print("listing title: %s" % (listing.get('title')))
-
-                    title_keywords = tokenize(listing['title'])
-                    #print("listing keywords: ")
-                    #print(title_keywords)
-
-                    # all product keywords contained in (subset of) listing title
-                    if set(product_keywords) < set(title_keywords):
-                        #print('match found:  %s' % (listing['title']))
-                        listing['matched'] = True
-                        matches.append(listing)
-
-                        #possible_matches.remove(listing) # listing can only be matched once
-                else:
-                    title_keywords = tokenize(listing['title'])
-
-                    # all the cases which could potentially belong to 2 different products
-                    if set(product_keywords) < set(title_keywords):
-                        print('listing found with 2 or more possible products: %s' % (listing['title']))
-
-    
-
-    #return len(matches)
-    return {'product_name' : product.get('product_name'), 'listings' : matches }
-
-
-def find_matches(product, listings):
-
-    #print("product name: %s" % (product.get('product_name')))
-
-    matches = []
-
-
-    # correct listing must be from same manufacturer and contain product model and family in title
-    key = format_key(product['manufacturer'])
-    #print("manufacturer key: %s" % (key))
-    product_keywords = tokenize(product['model']) + tokenize(product.get('family', ''))#+ tokenize(product['family'])
-    #print("product keywords: ")
-    #print(product_keywords)
-    possible_matches = listings.get(key, [])
-    for listing in possible_matches:#listings[key]:
-        #print("listing title: %s" % (listing.get('title')))
-
-        title_keywords = tokenize(listing['title'])
-        #print("listing keywords: ")
-        #print(title_keywords)
-
-        # all product keywords contained in (subset of) listing title
-        if set(product_keywords) < set(title_keywords):
-            #print('match found:  %s' % (listing['title']))
-            matches.append(listing)
-            possible_matches.remove(listing) # listing can only be matched once
-
-    return len(matches)
-
-    # each product can have multiple listings
-    # a listing may match at most one product
-
-    # different manufacturer does NOT guarantee they aren't a match
-    # Nikon vs Nikon Electronic, Konika Minolta vs Minolta
-    # tradeoff is less recall for much faster search (being able to use manufacturer as key)
-    # example:
-    # product:
-    #{"product_name":"Nikon_Coolpix_L22","manufacturer":"Nikon","model":"L22","family":"Coolpix","announced-date":"2010-02-02T19:00:00.000-05:00"}
-    #matching listing:
-    # {"title":"Nikon Coolpix L22 12.1MP Digital Camera with 3.6x Optical Zoom and 3.0-Inch LCD (Black)","manufacturer":"Nikon Electronic Imaging","currency":"CAD","price":"129.99"}
-
-    # ACCESORY bundle that doesn't match (or does it?):
-    # {"title":"Nikon Coolpix L22 12.0MP Digital Camera with 3.6x Optical Zoom and 3.0-Inch LCD (Silver) 4GB BigVALUEInc Accessory Saver NiMH Bundle","manufacturer":"Nikon","currency":"USD","price":"107.95"}
-
-
-    # without manufacturer as key, bestcase runtime is n^2
-    # with manufacturer, bestcase runtime is n
-    # determine recall while using manufacturer as key
-
-
-    # match criteria:
-    # same manufacturer
-    # all keywords in model, family are present in listing title
-    # price is within acceptable variance of other listings?
-
-    """
-    Product
-    {
-    "product_name": String // A unique id for the product
-    "manufacturer": String
-    "family": String // optional grouping of products
-    "model": String
-    "announced-date": String // ISO-8601 formatted date string, e.g. 2011-04-28T19:00:00.000-05:00
-    }
-    Listing
-    {
-    "title": String // description of product for sale
-    "manufacturer": String // who manufactures the product for sale
-    "currency": String // currency code, e.g. USD, CAD, GBP, etc.
-    "price": String // price, e.g. 19.99, 100.00
-    }
-
-    """
-
-
 def write_result(results):
     with open('./results.txt', 'w') as f:
         for result in results:
-            f.write(str(result))
+            f.write(json.dumps(result) + "\n\n")
+
+# True if set(a) < set(b)
+def is_subset(a, b):
+    for item in a:
+        if item not in b: return False
+
+    return True
+
+def find_best_match(listing, products):
+
+    # a possible match will be from the same manufacturer and have all tokenized (see tokenize()) keywords from the product group and family in the listing title
+    # two ways methods of reducing false positives:
+    #   1. product models with suffixes (e.g., WG-1 GPS) will score higher than those without (WG-1)
+    #   2. products with 2 or more same-score matches will be discarded (likely to be accessories for the product)
 
 
+
+    manufacturer_key = format_key(listing['manufacturer'])
+
+    title_keywords = (tokenize(listing['title']))
+
+    threshold_score = 0#2 # minimum score needed for a match
+
+    # product must be from same manufacturer
+
+    for key in products:
+        if key in manufacturer_key or manufacturer_key in key:
+
+            possible_matches = products.get(key, [])
+
+            for product in possible_matches:
+                #model = format_key(product.get('model'))
+                #family = format_key(product.get('family'), '')
+
+                score = 0
+
+                product_keywords = (tokenize(product.get('model')) + tokenize(product.get('family', '')))
+                # model = tokenize(product.get('model'))
+                # family = tokenize(product.get('family', ''))
+
+                # model and family must be mentioned in the title 
+                #if model_string in title_string and family_string in title_string:
+                #if product_keywords < title_keywords:
+                if is_subset(product_keywords, title_keywords):
+                    #longer matches are scored higher (due to model suffixes and stuff being a better match)
+                    #score = len(model_string) + len(family_string)
+                    score = len(product_keywords)
+                    if score > threshold_score:
+                        threshold_score = score
+
+
+                        #listing['best_match'] = product.get('product_name')
+                        #listing['shared_keywords'] = shared_keywords
+
+                        # find the problem listings
+                    listing.setdefault('best_match', []).append("SCORE="+str(score)+product.get('product_name'))
+                        #listing.setdefault('shared_keywords', []).append(shared_keywords)
+
+
+                    #product_keywords = tokenize(product.get('model')) + tokenize(product.get('family', '')) #+ tokenize(product.get('product_name'))
+                    #shared_keywords = set(product_keywords).intersection(`)
+                    #if set(product_keywords) < set(title_string):
+
+                        #score +=2
+                    # if product.get('family', False): # bonus to score if optinional family name is included and matched
+                    #     score +=1
+                    #score = len(shared_keywords)
+                    # if score >= threshold_score:
+                    #     threshold_score = score
 
 def main(products_path, listings_path):
 
     # test_keys(listings_path)
     start_time = time.time()
     
-    products = create_list(products_path) # list
-
-    listings = create_dict(listings_path) # dict
-
-    n_matched_listings = 0
-
-    results = []
-
     # start by mapping the manufacturer - correct listings must at least be from same manufacturer
-    # since this is the best criteria, create a hashmap using manufacturer as key, then we only have the check the listing from the same manufacturer for each product
+    # since this is the best criterion, create a hashmap using manufacturer as key, then we only have the check the listing from the same manufacturer for each product
     # worst case runtime is n^2 (all listings are same manufacturer), best case is n (1 listing per manufacturer)
 
+    products = create_dict(products_path) 
+
+    listings = create_list(listings_path) 
+
+    results = [] #dict by product name
+
+    n = 0
     
-    for product in products:
-    #for i in range(len(products)):
-        #print("\n-------%d--------\n" % (i))
-        #product = products[i]
-        #n_matched_listings += find_matches_deep(product, listings)
-        result = find_matches_deep(product, listings)
-        n_matched_listings += len(result['listings'])
-        results.append(result)
+    # iterate each listing and find its best product match
+    for listing in listings:
+        find_best_match(listing, products)
+        # if listing.get('best_match'):
+        #     n+=1
+        #     results.append({'listing' : listing['title'], 'product' :  listing.get('best_match'), 'keywords' : listing.get('shared_keywords')})
+
+        # find out which ones are finding matches for multiple products.. likely to be problem areas
+        if len(listing.get('best_match', [])) > 1:
+            n+=1
+            results.append({'listing' : listing['title'], 'product_matches' :  listing.get('best_match'), 'keywords' : listing.get('shared_keywords')})
+
+    print("%d product listings matched" % (n))
+    # for product in products:
+    # #for i in range(len(products)):
+    #     #print("\n-------%d--------\n" % (i))
+    #     #product = products[i]
+    #     #n_matched_listings += find_matches_deep(product, listings)
+    #     result = find_matches_deep(product, listings)
+    #     n_matched_listings += len(result['listings'])
+    #     results.append(result)
 
     #print results
-    print("%d matches found for %d products" % (n_matched_listings, len(products)))
+    #print("%d matches found for %d products" % (n_matched_listings, len(products)))
 
     write_result(results)
 
